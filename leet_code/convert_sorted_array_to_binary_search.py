4155d0f5d3eba18406d5d289da14361c5ce6435d
Mourya Kajjayam <mouryagren59@gmail.com>
2026-06-22 21:53:24 +0530

    Add files via upload

---
+++ leet_code/convert_sorted_array_to_binary_search.py
@@ -0,0 +1,16 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        def buildTree(left, right):
+            if left > right:
+                return None
+            mid = left + (right - left) // 2
+            root = TreeNode(nums[mid])
+            root.left = buildTree(left, mid - 1)
+            root.right = buildTree(mid + 1, right)
+            return root
+        return buildTree(0, len(nums) - 1)
